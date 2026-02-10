4a3117091da923e1dcdb3d9b0a8ec2ed2c34e55d
Alex Folland <lexlexlex@gmail.com>
2026-02-10 15:35:11 -0800

    prevent error when ExpansionLandingPageMinimapButton doesn't exist

--- MissionReportButtonHider.lua
+++ MissionReportButtonHider.lua
@@ -1,1 +1,3 @@
-hooksecurefunc(ExpansionLandingPageMinimapButton, "Show", function(self, ...) self:Hide() end)
+if ExpansionLandingPageMinimapButton and ExpansionLandingPageMinimapButton.Show then
+    hooksecurefunc(ExpansionLandingPageMinimapButton, "Show", function(self, ...) self:Hide() end)
+end
